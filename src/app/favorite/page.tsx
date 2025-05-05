ae8fd9ceeef5e304ed441a2767552419a9d757b3
Diogenes <paolofersantella@gmail.com>
2025-05-05 14:48:10 -0600

    optimizando Favorite-Page

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,55 +1,13 @@
-"use client"
+import GridFavorite from "../components/functionality/GridFavorite";
 
-import { useContext } from "react"
-import GridCharacters from "../components/functionality/GridCharacters"
-import { FavoriteContext } from "../context/FavoriteContext"
-import { Spinner } from "../components/ui/Spinner"
-import { FaHome } from "react-icons/fa"
-import { useRouter } from "next/navigation";
-
-const Page = () => {
-
-  const favoriteContext = useContext(FavoriteContext);
-
-  const router = useRouter();
-
-  if(!favoriteContext){
-    throw new Error('Context not found');
-  }
-
-  const {favoriteCharacters} = favoriteContext;
-
-  const handleHome = ()=>{
-    router.push('/');
-  }
+const page = () => {
 
   return (
     <>
-    <div className="flex absolute top-0 p-2 right-0 justify-center items-center">
-            <div>
-            <FaHome 
-                    size={30} 
-                    className="hover:bg-[#97CE4C] m-2 rounded-full transition-colors duration-300 p-1" 
-                    onClick={handleHome}
-              />
-            </div> 
-    
-    </div> 
-    {
-      (favoriteCharacters.length > 0) 
-
-      ?(<GridCharacters characters={favoriteCharacters}/>)
-      : (
-          <div className=" h-[80dvh] flex justify-center">
-              <Spinner/>
-          </div>
-        )
-    }
-    
-    
+        <GridFavorite/>  
     </>
 
   )
 }
 
-export default Page;
+export default page;
